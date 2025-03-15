2496fe1bf374534408666f42eb9e5599fa172de3
Abdullah Aly <abdullahaly.dev@gmail.com>
2025-03-15 06:44:14 +0200

    test(api): Add authentication api tests

---
+++ test/api/auth.test.ts
@@ -0,0 +1,72 @@
+// helpers
+import { createHonoApp } from "../../src/helpers/hono.helper";
+
+// repository
+import { fetchAuthRepository } from "../../src/api/v1/auth/auth.repository";
+
+// routes
+import { authRoute } from "../../src/api/v1/auth/auth.route";
+
+const env = {
+  server: {
+    incoming: {
+      socket: {
+        remoteAddress: "127.0.0.1",
+        remotePort: 8080,
+        remoteFamily: "IPv4",
+      },
+    },
+  },
+};
+
+describe("authentication/authorization api", () => {
+  const app = createHonoApp({
+    routes: [authRoute],
+    port: 9000,
+    hostname: "localhost",
+    debug: false,
+  });
+
+  test("create new authentication ticket", async () => {
+    const response = await app.request(
+      "http://localhost:9000/v1/auth/",
+      {
+        method: "POST",
+        body: JSON.stringify({ email: "john.deo@trychain.com" }),
+        headers: {
+          "content-type": "application/json",
+        },
+      },
+      env,
+    );
+
+    const data = await response.json();
+
+    expect(response.status).toBe(201);
+    expect(data).toHaveProperty("message");
+    expect(data.entry).toHaveProperty("id");
+    expect(data.entry).toHaveProperty("email");
+    expect(data.entry).toHaveProperty("country");
+    expect(data.entry).toHaveProperty("city");
+  });
+
+  test("verify authentication ticket", async () => {
+    const auth = await fetchAuthRepository({ email: "john.deo@trychain.com" });
+    const response = await app.request(
+      `http://localhost:9000/v1/auth/${auth.id}`,
+      {
+        method: "POST",
+        body: JSON.stringify({ password: auth.password }),
+        headers: {
+          "content-type": "application/json",
+        },
+      },
+      env,
+    );
+
+    const data = await response.json();
+
+    expect(response.status).toBe(200);
+    expect(data).toHaveProperty("message");
+  });
+});
