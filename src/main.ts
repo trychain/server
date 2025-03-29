1b1175d5e29ad8f7442d68b26f1ba86edfc8c91c
Abdullah Aly <abdullahaly.dev@gmail.com>
2025-03-29 05:27:59 +0200

    refactor: Add user and session routes with debug mode enabled

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { createHonoApp } from "./helpers/hono.helper";
 
 // routes
 import { authRoute } from "./api/v1/auth/auth.route";
+import { userRoute } from "./api/v1/user/user.route";
+import { sessionRoute } from "./api/v1/session/session.route";
 
 createHonoApp({
-  routes: [authRoute],
+  routes: [authRoute, userRoute, sessionRoute],
   port: 8000,
+  debug: true,
 });
